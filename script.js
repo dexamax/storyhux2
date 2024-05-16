37bd6672f98fe91548b7ba9721a92fab11c69192
Kathalanthe <hshalika49@gmail.com>
2024-05-16 09:57:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+redButton.addEventListener('click', function() {
+    // Redirect to the red page
+    window.location.href = 'https://www.highcpmgate.com/jbjapbqei?key=299e2b335b91cd6ba5ccfd5cb4e2a3ae';
+    // Store the state of the red button
+    sessionStorage.setItem('redButtonClicked', true);
+});
